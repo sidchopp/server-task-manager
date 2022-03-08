8cf3582264a2edcdcc99c3ca300c26091b4c576d
Sid <sidchopp@gmail.com>
2022-03-07 17:33:15 -0700

    Mongoose mongoDB env setup

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect');
+
+// mongoDB uri from .env
+require('dotenv').config();
 
 //Middleware to get json data in req.body or we will get req.body as undefined
 app.use(express.json());
@@ -11,6 +14,16 @@ app.use("/api/v1/tasks", tasks)
 
 
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
-})
+// Our server runs ONLY after it is connected with our mongoDB database
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI)
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}...`);
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
